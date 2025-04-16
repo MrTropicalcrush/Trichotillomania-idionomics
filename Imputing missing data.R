1d9dafe141c6f371c337f0b79029dad222739328
MrTropicalcrush <william.li2@myacu.edu.au>
2025-04-16 15:06:59 +1000

    Handling missing data.

--- Imputing missing data.R
+++ Imputing missing data.R
@@ -1,2 +1,15 @@
+# Handling missing data using longitidunal imputation
+
 library(idionomics)
-imputated_data <- imputatron_2000()
+library(dplyr)
+
+impute_these <- c("AffAnx","AffBored","CogWorrieAway","CogFix","SensHand","SenseHair","AttThts","ValueImp","BehvStim","MinutesPulling")
+
+trich_data <- trich_data %>%
+                    mutate(across(all_of(impute_these), ~ as.numeric(as.character(.))))
+
+imputated_data <- imputatron_2000(data= trich_data, id_col = "ID", time_col = "Time", cols_of_interest = impute_these)
+
+final_data <- imputated_data$imputed_df
+
+
